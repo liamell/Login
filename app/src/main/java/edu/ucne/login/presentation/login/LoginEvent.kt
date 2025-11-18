48319e163702f307585131e1f77b994eed7d93df
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-18 19:32:51 -0400

    loginEvent

--- app/src/main/java/edu/ucne/login/presentation/login/LoginEvent.kt
+++ app/src/main/java/edu/ucne/login/presentation/login/LoginEvent.kt
@@ -1,5 +1,6 @@
 package edu.ucne.login.presentation.login
 
+
 sealed class LoginEvent {
     data class UserNameChanged(val value: String): LoginEvent()
     data class PasswordChanged(val value: String): LoginEvent()
